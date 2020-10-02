894504c4dd25e7a21c79c6e14414b96d511b7b08
Andrew Siegel <siegelal7@gmail.com>
2020-10-01 20:27:03 -0400

    delete btn wont work on heroku page... testing

--- server.js
+++ server.js
@@ -50,46 +50,44 @@ app.post("/api/notes", function (req, res) {
   res.json(req.body);
 });
 
-// app.delete("/api/notes/:id", function (req, res) {
-//   const currentId = req.params.id;
-
-//   let newArr = dbJsonData.filter((i) => i.id !== currentId);
-//   console.log(newArr);
-//   fs.writeFile(
-//     path.join(__dirname, "./db/db.json"),
-//     JSON.stringify(newArr),
-//     function (err) {
-//       if (err) throw err;
-//       console.log("successfully deleted from db");
-//     }
-//   );
-//   console.log(dbJsonData);
-//   res.json({
-//     ok: "true",
-//   });
-// });
-
 app.delete("/api/notes/:id", function (req, res) {
-  let noteID = req.params.id;
-  fs.readFile(path.join(__dirname, "./db/db.json"), "utf-8", function (
-    err,
-    data
-  ) {
-    if (err) throw err;
-    const json = JSON.parse(data);
-    const newJson = json.filter((note) => note.id !== noteID);
-    fs.writeFile(
-      path.join(__dirname, "./db/db.json"),
-      JSON.stringify(newJson),
-      function (err) {
-        if (err) throw err;
-        console.log("successfully deleted, and wrote to db");
-      }
-    );
-  });
-  res.sendStatus(200);
-  // can also do: res.json({ ok: "true" });
+  const currentId = req.params.id;
+
+  let newArr = dbJsonData.filter((i) => i.id !== currentId);
+  console.log(newArr);
+  let newFile = fs.writeFile(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify(newArr),
+    function (err) {
+      if (err) throw err;
+      console.log("successfully deleted from db");
+    }
+  );
+  console.log(dbJsonData);
+  res.json(newFile);
 });
+
+// app.delete("/api/notes/:id", function (req, res) {
+//   let noteID = req.params.id;
+//   fs.readFile(path.join(__dirname, "./db/db.json"), "utf-8", function (
+//     err,
+//     data
+//   ) {
+//     if (err) throw err;
+//     const json = JSON.parse(data);
+//     const newJson = json.filter((note) => note.id !== noteID);
+//     fs.writeFile(
+//       path.join(__dirname, "./db/db.json"),
+//       JSON.stringify(newJson),
+//       function (err) {
+//         if (err) throw err;
+//         console.log("successfully deleted, and wrote to db");
+//       }
+//     );
+//   });
+//   res.sendStatus(200);
+//   // can also do: res.json({ ok: "true" });
+// });
 
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/index.html"));
